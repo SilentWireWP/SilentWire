e996bc1b2dfd5076f49ea6e37078b2436241972b
SilentWireWP <w97.pske@gmail.com>
2025-05-07 02:14:20 +0200

    Delete patches directory

--- patches/update_version_and_spawnfix.js
+++
@@ -1,6 +0,0 @@
-GameHooks.onGameStart.push(() => {
-  const version = document.getElementById("version-info");
-  if (version) version.textContent = "Version: Alpha 0.0.2";
-  nullborns.length = 0;
-  nullborns.push({ x: 5, y: 5 });
-});
